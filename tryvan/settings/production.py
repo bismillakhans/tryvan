b75fa158d11cf84bd0f4dee71daf8ff4a468a4a0
bismillakhans <bismillakhans@gmail.com>
2019-08-17 08:42:50 +0530

    local change

--- tryvan/settings/production.py
+++ tryvan/settings/production.py
@@ -9,10 +9,11 @@ DATABASES = {
       default = config('DATABASE_URL'))
 }
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware', ]
-django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+django_heroku.settings(locals())
